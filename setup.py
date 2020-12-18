8713c3ec160f14cd3dcb1ed3bacffc9b371bb502
Alexie Staffer <alexie.staffer@ebi.ac.uk>
2020-12-18 17:12:58 +0000

    0.5.0 added to test.pypi.org

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="json-converter",
+    version="0.5.0",
+    author="Alexie Staffer ",
+    author_email="alexie.staffer@ebi.ac.uk",
+    description="json-converter is a tool for translating/converting a JSON document into another JSON document with a different structure.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ebi-ait/json-converter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.8',
+)
